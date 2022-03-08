eabc8e6dbcf32240ffdec796aa2c3242891cfe79
Gabriel <gabriel.ferreira2002@hotmail.com>
2022-03-07 22:55:05 -0300

    1

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio1.js
@@ -0,0 +1,41 @@
+//exercicio 1
+
+let a = 10;
+let b = 5;
+
+let adicao = a + b;
+let subtracao = a - b;
+let multi = a * b;
+let divisao = a / b;
+let mod = a % b;
+
+console.log("exercício 1: ", adicao, subtracao, multi, divisao, mod);
+
+
+//exercicio 2
+const mai1 = 4;
+const mai2 = 5;
+
+if(mai1 > mai2){
+    console.log("exercício 2: ", mai1) 
+}
+else{
+    console.log("exercício 2: ", mai2)
+}
+
+
+//exercicio 3
+const a3 = 7;
+const b3 = 23;
+const c3 = 12;
+
+if(a3 > b3 && a3 > c3){
+    console.log("exercício 3: ", a3, "é o maior número")
+}else if(b3 > a3 && b3 > c3){
+    console.log("exercício 3: ", b3, "é o maior número")
+}else{
+    console.log("exercício 3: ", c3, "é o maior número")
+}
+
+
+//exercicio 4
