bc72346bf984005024245945d6894ee60b630875
Gabriel <gabriel.ferreira2002@hotmail.com>
2022-03-09 21:38:27 -0300

    indentação

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/ex3.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/ex3.js
@@ -1,21 +1,19 @@
-let array = ['java', 'javascript', 'python', 'html', 'css'];
+let array = ["java", "javascript", "python", "html", "css"];
 
 let bigword = array[0];
 let smallword = array[0];
 
-
-for(let i =0; i < array.length; i++){
-    if(array[i].length > bigword.length){
-        bigword = array[i];
-    }
+for (let i = 0; i < array.length; i++) {
+  if (array[i].length > bigword.length) {
+    bigword = array[i];
+  }
 }
 
-for(let i = 0; i < array.length; i++){
-    if(array[i].length < smallword.length){
-        smallword = array[i];
-    }
+for (let i = 0; i < array.length; i++) {
+  if (array[i].length < smallword.length) {
+    smallword = array[i];
+  }
 }
-
 
 console.log(bigword);
 console.log(smallword);
--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exerciciosaula.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exerciciosaula.js
@@ -2,8 +2,8 @@
 // A soma de 50 a 100 é: X.
 let number = 0;
 
-for(i=50; i<=100; i++){
-    number += i;
+for (i = 50; i <= 100; i++) {
+  number += i;
 }
 console.log(number);
 
@@ -12,48 +12,36 @@ console.log(number);
 
 let number2 = 0;
 
-for(index =1; index <= 125; index++){
-    if(index % 3 === 0){
-        number2 += 1
-
-    }
+for (index = 1; index <= 125; index++) {
+  if (index % 3 === 0) {
+    number2 += 1;
+  }
 }
-if(number2 ===50){
-    console.log("deu 50");
+if (number2 === 50) {
+  console.log("deu 50");
 }
 console.log(number2);
 
 // 3. Crie um algoritmo que simula o jogo “pedra, papel e tesoura” levando em consideração que são apenas duas pessoas jogando e imprima o resultado no formato:
 // “Jogador 1 vence” ou “Empate” ou “Jogador 2 vence”.
 
-jogador1 =  Math.floor(Math.random() * (3-1) + 1) ;
-jogador2 =  Math.floor(Math.random() * (3-1) + 1) ;
+jogador1 = Math.floor(Math.random() * (3 - 1) + 1);
+jogador2 = Math.floor(Math.random() * (3 - 1) + 1);
 
-if(jogador1 === jogador2){
-    console.log("empate")
-}else if(jogador1===1 && jogador2 ===2){
-    console.log("Jogador 1 ganha")
-}else if(jogador1===2 && jogador2 ===3){
-    console.log("Jogador 1 ganha")
-}else if(jogador1===3 && jogador2 ===1){
-    console.log("Jogador 1 ganha")
-}else{
-    console.log("jogador 2 ganha")
+if (jogador1 === jogador2) {
+  console.log("empate");
+} else if (jogador1 === 1 && jogador2 === 2) {
+  console.log("Jogador 1 ganha");
+} else if (jogador1 === 2 && jogador2 === 3) {
+  console.log("Jogador 1 ganha");
+} else if (jogador1 === 3 && jogador2 === 1) {
+  console.log("Jogador 1 ganha");
+} else {
+  console.log("jogador 2 ganha");
 }
 
 // 4. Desenvolva um algoritmo que verifica se a pessoa é maior ou menor de idade.
 // Imprima no console seguindo o exemplo: “A pessoa é maior de idade”.
 
-
 // 5. Crie um algoritmo que recebe a idade de Marcella, Carlos e Camila e imprima quem é a mais nova no formato:
 // “Pessoa” é a mais nova.
-
-
-
-
-
-
-
-
-
-
