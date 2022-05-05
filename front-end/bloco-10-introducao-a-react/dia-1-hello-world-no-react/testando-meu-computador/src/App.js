e8ffa574745019660f24b1d0406f51bf835d3571
Gabriel <gabriel.ferreira2002@hotmail.com>
2022-05-05 18:47:33 -0300

    exercicio fixação

---
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/testando-meu-computador/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import Component from './Component';
+
+class App extends React.Component {
+  render() {
+    return(
+      <div>
+        <Component />
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/testando-meu-computador/src/Component.js
@@ -0,0 +1,12 @@
+import React from 'react';
+class Component extends React.Component {
+    render() {
+        return(
+            <div>
+                <h1>Gabriel Gomes</h1>
+                <p>Tenho 19 anos</p>
+            </div>
+        )
+    }
+}
+export default Component; 
